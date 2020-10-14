3b1a696890490c45ec9b127a59427342f6a66a4c
Vic Flores <vicsito2014@gmail.com>
2020-10-14 17:03:58 -0600

    ctx was added

--- src/server.js
+++ src/server.js
@@ -1,17 +1,32 @@
 import { ApolloServer } from 'apollo-server';
 import glue from 'schemaglue';
+import jwt from 'jsonwebtoken';
 
 const { schema, resolver } = glue('src/graphql');
 
 // DB connection
 require('./bd');
 
+require('dotenv').config({ path: 'variables.env' });
+
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers: resolver,
+  context: ({ req }) => {
+    const token = req.headers.authorization || '';
+
+    if (token) {
+      try {
+        const employee = jwt.verify(token.replace('Bearer ', ''), process.env.SECRET);
+        return { employee };
+      } catch (error) {
+        return new Error('Token incorrect');
+      }
+    }
+  },
 });
 
-server.listen({ port: process.env.PORT || 3000 })
+server.listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => {
     console.log(`Server running on ${url}`);
   })
