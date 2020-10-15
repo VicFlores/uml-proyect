64928fb88b4d394a2de353002c349766d585133f
Vic Flores <vicsito2014@gmail.com>
2020-10-15 10:01:34 -0600

    trim was added

--- src/models/Employees.js
+++ src/models/Employees.js
@@ -1,10 +1,10 @@
 import { Schema, model } from 'mongoose';
 
 const employeeSchema = new Schema({
-  name: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true, trim: true },
 });
 
 module.exports = model('Employees', employeeSchema);
