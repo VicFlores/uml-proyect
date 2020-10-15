08eaf6676ae8fcbf6a657294a57311ee272684c9
Vic Flores <vicsito2014@gmail.com>
2020-10-15 10:03:20 -0600

    Slint corrections

--- src/models/Employees.js
+++ src/models/Employees.js
@@ -3,7 +3,9 @@ import { Schema, model } from 'mongoose';
 const employeeSchema = new Schema({
   name: { type: String, required: true, trim: true },
   lastName: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true },
+  email: {
+    type: String, required: true, unique: true, trim: true,
+  },
   password: { type: String, required: true, trim: true },
 });
 
